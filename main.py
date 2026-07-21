d7bd3453f5053529b3c2826c1034ccb98172a0a1
Jay Raj <haldarjayraj@gmail.com>
2026-07-21 16:37:19 +0530

    Updated

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ st.subheader("Brewed with streamlit")
 st.text("Welcomt to your first interactive app")
 st.write("Choose your favourite variety of movie")
 
-movie = st.selectbox()
+movie = st.selectbox("Select your favourite movie: ", ['Odyssey', 'Radhe', 'Interstellar']) #jo bhi values ham select karenge vo iss movie variable me store ho jayegi 
+st.write(f"You chose {movie}. Excellent choice")
+st.success('Your movie has been initialized')
+
+
