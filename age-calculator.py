2ad6ce25716ceb492a930e769b89bd3d4a4fcb53
Jay Raj <haldarjayraj@gmail.com>
2026-08-03 00:23:20 +0530

    Ch 3 started

--- age-calculator.py
+++ age-calculator.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from datetime import date 
 st.title("AGE CALCULATOR USING STREAMLIT")
 
-dob = st.date_input("Enter your date of birth")
+dob = st.date_input("Enter your date of birth: ", value= date.today(), min_value=date(1900,1,1), max_value=date(3600,1,1))
+
+st.write(f"The entered date of birth is {dob} .")
+
+
+
+
+
---
+++ ch3.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Chai Taste Poll ")
+
+col1,col2 = st.columns(2) #two columns whose names are col1 and 2
+with col1: #har column bhi apne aap me ek page ki tarah treat hota hai 
+    st.header("Masala Chai")
+    vote1 = st.button("Vote masala chai")
+with col2:
+    st.header("Adrak Chai")
+    vote2 = st.button("Vote adrak chai") 
+
+if vote1:
+    st.success("Thanks for voting masala chai")
+elif vote2:
+    st.success("Thanks for voting adrak chai")
