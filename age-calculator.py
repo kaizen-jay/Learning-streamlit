05ce463ad9ae1030965182cdbc8381889ae2804b
Jay Raj <haldarjayraj@gmail.com>
2026-08-10 23:31:37 +0530

    making the age calculator

--- age-calculator.py
+++ age-calculator.py
@@ -1,12 +1,15 @@
 import streamlit as st
-from datetime import date 
+from datetime import date
+
 st.title("AGE CALCULATOR USING STREAMLIT")
-
-dob = st.date_input("Enter your date of birth: ", value= date.today(), min_value=date(1900,1,1), max_value=date(3600,1,1))
-
-st.write(f"The entered date of birth is {dob} .")
-
-
+dob= st.date_input("Enter your Date of Birth: ", min_value=date(1900,1,1), max_value=date.today())
+today = date.today()
+age = today.year - dob.year
+st.success(f"Your age is {age} years")
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
 
 
 
