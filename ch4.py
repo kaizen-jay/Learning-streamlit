1c87d35b2d63dd2099980c671f428a6a5fcd5589
Jay Raj <haldarjayraj@gmail.com>
2026-08-16 00:13:52 +0530

    Added the dataframes

--- ch4.py
+++ ch4.py
@@ -13,5 +13,10 @@ if file:
 # If we wanted to pick out all the unique values form a column we can just use the pandas function just like:
 
 if file:
-    sex = df["sex"].unique()
-    gender = st.selectbox("Filter by gender", sex) #means select box ko do parameters lagte hai first is filter by gender and parameter diya sex.
+    genders = df["sex"].unique()
+    gender = st.selectbox("Filter by gender", genders) #means select box ko do parameters lagte hai first is filter by gender and parameter diya genderr.
+
+#Ham dataframes ke andar unique values ko select kar sakte hai and aur uska bhi dataframe bana sakte hai  
+    filtered_data = df[df["sex"] == gender]   #matlab ki ham sex dataframe ko select karna chahte hai . and agar is sex ki value equal ho gender ke andar to iss poore ko ham dataframe bana denge . Isse hoga ye ki poora filtered data ho jayega (more in pandas section) 
+    #Now to display the outcome:
+    st.dataframe(filtered_data) #matlab ki mujhe jo dataframe display karna hai vo de denge
