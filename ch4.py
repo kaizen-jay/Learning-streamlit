31be593746376d713ec651f56e99539244fa7d8a
Jay Raj <haldarjayraj@gmail.com>
2026-08-15 23:53:12 +0530

    Updated some changes;

--- ch4.py
+++ ch4.py
@@ -6,7 +6,11 @@ if file:
     df = pd.read_csv(file)
     st.subheader("Data preview")
     st.dataframe(df) #Pandas ke sath streamlit ka kaafi close relation hai 
-if file;
+if file:
     st.subheader("Summary of the data")
     st.write(df.describe())
+# this is somethibn intersting to do with pandas
+# If we wanted to pick out all the unique values form a column we can just use the pandas function just like:
+
+if file:
     
