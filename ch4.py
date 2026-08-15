417e86693bac3b80291f500adcb21ec2d0c5da20
Jay Raj <haldarjayraj@gmail.com>
2026-08-15 23:39:15 +0530

    Updates

--- ch4.py
+++ ch4.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.title("Chai sales dashboard")
-file = st.file_upload("Upload your csv file", type=[]"csv"])
+file = st.file_uploader("Upload your csv file", type=["csv"])
 if file:
     df = pd.read_csv(file)
     st.subheader("Data preview")
