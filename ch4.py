bc77ff46685cacdb7f4e0eda766a491665dabf30
Jay Raj <haldarjayraj@gmail.com>
2026-08-21 17:39:21 +0530

    Tweeks

--- ch4.py
+++ ch4.py
@@ -7,17 +7,17 @@ if file:
     st.subheader("Data preview")
     st.dataframe(df) #Pandas ke sath streamlit ka kaafi close relation hai 
 if file:
-    st.subheader("Summary of the data")
+    st.subheader("Summary stats")
     st.write(df.describe())
 # this is somethibn intersting to do with pandas
 # If we wanted to pick out all the unique values form a column we can just use the pandas function just like:
 
 if file:
-    genders = df["sex"].unique()
-    gender = st.selectbox("Filter by gender", genders) #means select box ko do parameters lagte hai first is filter by gender and parameter diya genderr.
+    cities = df["City"].unique() # hamne genders variable ke andar ek 'sex' naam ka column le liya hai jiski saari unique values ham isme store kar rahe hai.   
+    selected_city = st.selectbox("Filter by cities", cities) #means select box ko do parameters lagte hai first is filter by gender and parameter diya genderr.
 
 #Ham dataframes ke andar unique values ko select kar sakte hai and aur uska bhi dataframe bana sakte hai  
-    filtered_data = df[df["sex"] == gender]   #matlab ki ham sex dataframe ko select karna chahte hai . and agar is sex ki value equal ho gender ke andar to iss poore ko ham dataframe bana denge . Isse hoga ye ki poora filtered data ho jayega (more in pandas section) 
+    filtered_data = df[df["City"] == selected_city] #matlab ki ham sex dataframe ko select karna chahte hai . and agar is sex ki value equal ho gender ke andar to iss poore ko ham dataframe bana denge . Isse hoga ye ki poora filtered data ho jayega (more in pandas section) 
     #Now to display the outcome:
     st.dataframe(filtered_data) #matlab ki mujhe jo dataframe display karna hai vo de denge
 
