1e529ac1d3b780c8ccd887966bb2ed953c34dc2a
Jay Raj <haldarjayraj@gmail.com>
2026-08-22 14:08:20 +0530

    Initializing the currency converter

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 <video src="./Chapter4.mp4" controls width="700"></video>
+ 
---
+++ ch5.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import requests
+
+ #pehle title denge, fir user ko bolenge ki amount choose karo then uske liye input box denge and for user ko bolenge ki kis currency me convert karna hai 
+
+st.title("LIVE CURRENCY CONVERTER")
+st.number_input("Enter the amount in INR: ")
