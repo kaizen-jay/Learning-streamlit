204f6bc840e4d64c9a927bf2940eae57cabf81aa
Jay Raj <haldarjayraj@gmail.com>
2026-09-02 23:55:35 +0530

    Tweeks

--- ch-1.py
+++ ch-1.py
@@ -8,6 +8,7 @@ st.write("Choose your favourite variety of movie")
 movie = st.selectbox("Select your favourite movie: ", ['Odyssey', 'Radhe', 'Interstellar']) #jo bhi values ham select karenge vo iss movie variable me store ho jayegi 
 st.write(f"You chose {movie}. Excellent choice")
 st.success('Your movie has been initialized')
+#tweeks........
 
 
 
